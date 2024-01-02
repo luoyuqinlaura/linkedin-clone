034f953bdbcf015b307d0cee18ab8cb05379e190
luoyuqinlaura <84111527+luoyuqinlaura@users.noreply.github.com>
2024-01-01 20:52:52 -0800

    create headerOption template

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import HeaderOption from './HeaderOption';
 
 const Header = () => {
   return (
@@ -10,13 +13,18 @@ const Header = () => {
           src="https://www.svgviewer.dev/static-svgs/495625/linkedin.svg"
           alt=""
         />
+
         <div className="header__search">
           {/* SearchIcon */}
           <SearchIcon />
           <input type="text"></input>
         </div>
       </div>
-      <div className="header__right"></div>
+
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+      </div>
     </div>
   );
 };
