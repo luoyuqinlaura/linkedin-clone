4114a7c08632b2b32f909139cac429818c2a79b0
luoyuqinlaura <84111527+luoyuqinlaura@users.noreply.github.com>
2024-01-01 21:26:05 -0800

    Finish header options

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ https://www.youtube.com/watch?v=QaYts9sPmcY
 1. devide the header into left and right parts
 2. for the left search part, using <b>MaterialUI</b>
 3. command + p for search file in the vscode
+
+4. add avatar now
--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,9 @@ import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
 import HomeIcon from '@mui/icons-material/Home';
 import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 import HeaderOption from './HeaderOption';
 
 const Header = () => {
@@ -24,6 +27,9 @@ const Header = () => {
       <div className="header__right">
         <HeaderOption Icon={HomeIcon} title="Home" />
         <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
       </div>
     </div>
   );
